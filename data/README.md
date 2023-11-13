c7a7c4500924263baa4b6e9294caf8a6533ac043
Kevin Bonham, PhD <kbonham@wellesley.edu>
2023-11-13 17:36:55 -0500

    finish eeg loading code

--- data/README.md
+++ data/README.md
@@ -2,10 +2,11 @@
 
 ## EEG VEP
 
-- `eeg_vep_khulaSA_3M.csv`: sheet 2 from `3M_SA_VEP_FINALIZED_11-4-23.xlsx`,
+- `eeg_vep_khulaSA_3m.csv`: sheet 2 from `3M_SA_VEP_FINALIZED_11-4-23.xlsx`,
   sent by Emma via slack
-- `eeg_vep_khulaSA_6M.csv`: sheet 2 from `6M_SA_VEP_FINALIZED_11-2-23.xlsx`,
+- `eeg_vep_khulaSA_6m.csv`: sheet 2 from `6M_SA_VEP_FINALIZED_11-2-23.xlsx`,
   sent by Emma via slack
-- `eeg_vep_khulaSA_12M.csv`: sheet 2 from `12M_SA_VEP_FINALIZED_11-9-23.xlsx`,
+- `eeg_vep_khulaSA_12m.csv`: sheet 2 from `12M_SA_VEP_FINALIZED_11-9-23.xlsx`,
   sent by Emma via slack
+  - Also renamed "subject-id" to "subject_id" to match others
 
--- src/EEGMicrobiome.jl
+++ src/EEGMicrobiome.jl
@@ -1,7 +1,36 @@
 module EEGMicrobiome
 
+export load_eeg
+
 using CSV
 using DataFrames
+using Chain
 
+const _subject_excludes = Set([
+    "191-29736014", # premature
+    "191-34303377", # premature
+])
+
+function load_eeg(datadir = "./data")
+    df = DataFrame()
+    for tp in ("3m", "6m", "12m")
+        path = joinpath(datadir, "eeg_vep_khulaSA_$tp.csv")
+        tpdf = @chain path begin
+            CSV.read(DataFrame)
+            subset!("infant_eye_anomaly_1yes_0no" => ByRow(x-> x != 1), 
+                    "subject_id"=> ByRow(x-> x ∉ _subject_excludes)
+            )
+            select!("subject_id"               => "subject",
+                    "age_$(tp)_vep" => "age", 
+                    "Number_Segs_Post-Seg_Rej" => "n_segments",
+                    r"^peak")
+            rename(col-> replace(col, "_$tp"=> ""), _)
+        end
+        tpdf.timepoint .= tp
+        append!(df, tpdf)
+    end
+    return df
+        
+end
 
 end
