03d9ac05b670b7173b27bf76f818d69ba45455b4
Kevin Bonham, PhD <kbonham@wellesley.edu>
2023-11-27 20:37:56 -0500

    more data loading

--- analysis/first_pass.jl
+++ analysis/first_pass.jl
@@ -2,7 +2,21 @@ using FeatureSetEnrichments
 using VKCComputing
 using EEGMicrobiome
 
+# load data
 eeg = load_eeg()
 mbo = load_microbiome(eeg.subject)
+
+load_taxonomic_profiles!(mbo)
+load_functional_profiles!(mbo)
+unique!(mbo, :seqprep)
+sort!(mbo, "age")
+subset!(mbo, "age"=> ByRow(!ismissing))
+# 3M concurrent EEG
+
+eeg3m = subset(eeg, "timepoint"=> ByRow(==("3m")))
+leftjoin!(eeg3m, select(unique(subset(DataFrame(get(taxa)), "age"=> ByRow(a-> a < 9)), :subject), "subject", "age"=>"stool_age", "sample"); on=:subject)
+subset!(eeg3m, "sample"=> ByRow(!ismissing))
+
+leftjoin!(eeg3m, unique(select(subset(mbo, "age"=>ByRow(<(6))), Not("age")), "subject"); on=:subject)
 
 
--- src/EEGMicrobiome.jl
+++ src/EEGMicrobiome.jl
@@ -2,12 +2,18 @@ module EEGMicrobiome
 
 export load_eeg,
        load_microbiome,
-       load_taxonomic_profiles
+       load_taxonomic_profiles!
+       load_functional_profiles!
 
 using CSV
 using DataFrames
 using Chain
 using VKCComputing
+using Preferences
+using BiobakeryUtils: metaphlan_profiles
+using ThreadsX
+using GLM
+
 
 const _subject_excludes = Set([
     "191-29736014", # premature
@@ -28,6 +34,7 @@ function load_eeg(datadir = "./data")
                     "Number_Segs_Post-Seg_Rej" => "n_segments",
                     r"^peak")
             rename(col-> replace(col, "_$tp"=> ""), _)
+            transform!("age"=> ByRow((a-> a / 52 * 12))=> "age") # convert to months
         end
         tpdf.timepoint .= tp
         append!(df, tpdf)
@@ -46,11 +53,92 @@ function load_microbiome(subjects)
     subset!(df, :seqprep_ids=> ByRow(s-> !ismissing(s) && base[s][:keep] == 1)) 
     df.seqprep = [base[rec][:uid] for rec in df.seqprep_ids]
     df.age = Union{Missing, Float64}[get(base[rec], :subject_age, missing) for rec in df.biospecimen_ids]
-    return df
+    return select(df, "subject", "biospecimen", "seqprep", "age")
     
 end
 
-function load_taxonomic_profiles(seqs)
+function load_taxonomic_profiles!(mbiome_tab)
+    seqs = mbiome_tab.seqprep
+
+    mgxdir = load_preference(VKCComputing, "mgx_analysis_dir")
+    files = filter(readdir(joinpath(mgxdir, "metaphlan"); join=true)) do file
+        m = match(r"^SEQ\d+", basename(file))
+        !isnothing(m) && endswith(basename(file), "_profile.tsv") && m.match ∈ seqs
+    end
+    taxa = mapreduce(vcat, files) do f
+        seqprep = replace(basename(f), r"_S\d+_profile\.tsv"=> "")
+        df = CSV.read(f, DataFrame; skipto = 5, header=["feature", "taxid", "abundance", "addtl_spec"])
+        df.seqprep .= seqprep
+        subset!(df, "feature"=> ByRow(f-> contains(f, "|s__")))
+        transform!(df, "feature"=> ByRow(f-> last(split(f, '|')))=>"feature")
+        select!(df, "seqprep", "feature", "abundance")
+    end
+    taxa_wide = unstack(taxa, "feature", "abundance")
+    foreach(names(taxa_wide)) do n
+        taxa_wide[!, n] = coalesce.(taxa_wide[!, n], 0.)
+    end
+    leftjoin!(mbiome_tab, taxa_wide; on=:seqprep)
+
 end
+
+function load_functional_profiles!(mbiome_tab)
+    seqs = mbiome_tab.seqprep
+
+    mgxdir = load_preference(VKCComputing, "mgx_analysis_dir")
+    files = filter(readdir(joinpath(mgxdir, "humann", "main"); join=true)) do file
+        m = match(r"^SEQ\d+", basename(file))
+        !isnothing(m) && endswith(basename(file), "_genefamilies.tsv") && m.match ∈ seqs
+    end
+    gfs = mapreduce(vcat, files) do f
+        seqprep = replace(basename(f), r"_S\d+_genefamilies\.tsv"=> "")
+        df = CSV.read(f, DataFrame; skipto = 2, header=["feature", "abundance"])
+        df.seqprep .= seqprep
+        subset!(df, "feature"=> ByRow(f-> !contains(f, '|')))
+        select!(df, "seqprep", "feature", "abundance")
+    end
+    gfs_wide = unstack(gfs, "feature", "abundance")
+    foreach(names(gfs_wide)) do n
+        gfs_wide[!, n] = coalesce.(gfs_wide[!, n], 0.)
+    end
+    leftjoin!(mbiome_tab, gfs_wide; on=:seqprep)
+end
+
+
+function runlms(indf, outfile, respcol, featurecols;
+                age_col = "age",
+                formula = term(:func) ~ term(respcol) + term(age_col) + term(:n_segments),
+        )
+        @debug "Respcol: $respcol"
+    lmresults = DataFrame(ThreadsX.map(featurecols) do feature
+        # @debug "Feature: $feature"
+
+        # ab = collect(indf[!, feature] .+ (minimum(indf[over0, feature])) / 2) # add half-minimum non-zerovalue
+        df = select(indf, respcol, age_col, "n_segments")
+        over0 = indf[!, feature] .> 0
+        
+        df.func = over0
+        # @debug "DataFrame: $df"
+
+        try
+            mod = glm(formula, df, Binomial(), LogitLink(); dropcollinear=false)
+            ct = DataFrame(coeftable(mod))
+            ct.feature .= feature
+            rename!(ct, "Pr(>|z|)"=>"pvalue", "Lower 95%"=> "lower_95", "Upper 95%"=> "upper_95", "Coef."=> "coef", "Std. Error"=>"std_err")
+            select!(ct, Cols(:feature, :Name, :))
+            return NamedTuple(only(filter(row-> row.Name == respcol, eachrow(ct))))    
+        catch e
+            @warn "hit $e for $feature"
+            return (; feature, Name = respcol, coef = NaN, std_err = NaN, z = NaN, pvalue = NaN, lower_95 = NaN, upper_95 = NaN, qvalue = NaN)
+        end
+    end)
+
+    subset!(lmresults, "pvalue"=> ByRow(!isnan))
+    DataFrames.transform!(lmresults, :pvalue => (col-> MultipleTesting.adjust(collect(col), BenjaminiHochberg())) => :qvalue)
+    sort!(lmresults, :qvalue)
+
+    CSV.write(outfile, lmresults)
+    lmresults
+end
+
 
 end
