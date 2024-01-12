6194dfa33d299a3b2503c76c6d414a5399a0915a
Kevin Bonham, PhD <kbonham@wellesley.edu>
2024-01-12 16:10:17 -0500

    more tweaks to RF

--- analysis/rf_species.jl
+++ analysis/rf_species.jl
@@ -1,8 +1,11 @@
+using CSV: partitions
+using Statistics
 using VKCComputing
 using EEGMicrobiome
 using FeatureSetEnrichments
 using DataFrames
 using CSV
+using Chain
 using CairoMakie
 using Preferences
 using ColorSchemes
@@ -23,10 +26,11 @@ load_taxonomic_profiles!(mbo)
 
 topspec = readlines("data/outputs/topspecies.txt")
 
-modelin = leftjoin(eeg, mbo; on=["subject", "timepoint"])
-dropmissing!(modelin)
-subset!(modelin, "seqprep" => ByRow(!ismissing))
-
+modelin = @chain eeg begin
+    leftjoin(mbo; on=["subject", "timepoint"])
+    dropmissing!
+    subset!("seqprep" => ByRow(!ismissing))
+end
 #-
 
 rng = StableRNG(1984)
@@ -40,7 +44,6 @@ r_min_samples_split = range(tree, :min_samples_split; lower=2, upper=10)
 r_max_depth         = range(tree, :max_depth; lower=1, upper=6)
 r_min_samples_leaf  = range(tree, :min_samples_leaf; lower=1, upper=5)
 
-cv = CV(; nfolds = 4, shuffle=true, rng)
 self_tuning_tree = TunedModel(
     model = tree,
     tuning = Grid(; resolution=5),
@@ -53,34 +56,69 @@ self_tuning_tree = TunedModel(
 summary_stats = DataFrame()
 
 for eeg_feat in names(modelin, r"peak_")
+    @info "Testing $eeg_feat"
+    pfolds = partition(unique(modelin.subject), 0.25, 0.25, 0.25)
+    trainfolds = map(ps -> findall(s-> s ∉ ps, modelin.subject), pfolds)
+    testfolds = map(ps -> findall(s-> s ∈ ps, modelin.subject), pfolds)
 
-    age_only_tuned = machine(self_tuning_tree, select(modelin, "eeg_age", "n_segments"), modelin[!, eeg_feat]);
-    plus_bugs_tuned = machine(self_tuning_tree, select(modelin, "eeg_age", "n_segments", topspec...), modelin[!, eeg_feat]);
-    bugs_only_tuned = machine(self_tuning_tree, select(modelin, "n_segments", topspec...), modelin[!, eeg_feat]);
+    predictions = copy(modelin)
+    
+    for (i, (train, test)) in enumerate(zip(trainfolds, testfolds))
+        @info "Fold $i"
+        age_only_pred = Union{Missing, Float64}[missing for _ in 1:size(modelin,1)]
+        age_only_tuned = machine(self_tuning_tree, select(modelin, "eeg_age", "n_segments"), modelin[!, eeg_feat]);
+        plus_bugs_tuned = machine(self_tuning_tree, select(modelin, "eeg_age", "n_segments", topspec...), modelin[!, eeg_feat]);
+        bugs_only_tuned = machine(self_tuning_tree, select(modelin, "n_segments", topspec...), modelin[!, eeg_feat]);
 
-    MLJ.fit!(age_only_tuned)
-    MLJ.fit!(plus_bugs_tuned)
-    MLJ.fit!(bugs_only_tuned)
+        MLJ.fit!(age_only_tuned; rows=train)
+        MLJ.fit!(plus_bugs_tuned; rows=train)
+        MLJ.fit!(bugs_only_tuned; rows=train)
 
-    age_only_predictions = MLJ.predict(age_only_tuned)
-    plus_bugs_predictions = MLJ.predict(plus_bugs_tuned)
-    bugs_only_predictions = MLJ.predict(bugs_only_tuned)
+        age_only_predictions = MLJ.predict(age_only_tuned; rows=test)
+        plus_bugs_predictions = MLJ.predict(plus_bugs_tuned; rows=test)
+        bugs_only_predictions = MLJ.predict(bugs_only_tuned; rows=test)
+
+        age_only_dfinsert  = Union{Missing, Float64}[missing for _ in 1:size(modelin, 1)]
+        plus_bugs_dfinsert = Union{Missing, Float64}[missing for _ in 1:size(modelin, 1)]
+        bugs_only_dfinsert = Union{Missing, Float64}[missing for _ in 1:size(modelin, 1)]
+
+        age_only_dfinsert[test]  .= age_only_predictions 
+        plus_bugs_dfinsert[test] .= plus_bugs_predictions
+        bugs_only_dfinsert[test] .= bugs_only_predictions
+        
+        predictions[!, "age_only_fold$i"] = age_only_dfinsert
+        predictions[!, "plus_bugs_fold$i"] = plus_bugs_dfinsert
+        predictions[!, "bugs_only_fold$i"] = bugs_only_dfinsert
+         
+        age_only_err = mape(age_only_predictions, modelin[test, eeg_feat])
+        age_only_r² = cor(age_only_predictions, modelin[test, eeg_feat])^2
+
+        plus_bugs_err = mape(plus_bugs_predictions, modelin[test, eeg_feat])
+        plus_bugs_r² = cor(plus_bugs_predictions, modelin[test, eeg_feat])^2
+
+        bugs_only_err = mape(bugs_only_predictions, modelin[test, eeg_feat])
+        bugs_only_r² = cor(bugs_only_predictions, modelin[test, eeg_feat])^2
+        append!(summary_stats, DataFrame(
+             feature = fill(eeg_feat, 3),
+             model = ["age_only", "bugs_only", "age_plus_bugs"],
+             r² = [age_only_r², bugs_only_r², plus_bugs_r²],
+             mape = [age_only_err, bugs_only_err, plus_bugs_err],
+             fold = fill(i, 3),
+        ))
+    end
+    
+    transform!(predictions, 
+        AsTable(r"age_only_fold")=> ByRow(nt-> mean(skipmissing(values(nt))))=> "age_only_mean_pred",
+        AsTable(r"plus_bugs_fold")=> ByRow(nt-> mean(skipmissing(values(nt))))=> "plus_bugs_mean_pred"
+    )    
+    age_only_predictions = predictions.age_only_mean_pred
+    plus_bugs_predictions = predictions.plus_bugs_mean_pred
 
     age_only_err = mape(age_only_predictions, modelin[!, eeg_feat])
     age_only_r² = cor(age_only_predictions, modelin[!, eeg_feat])^2
 
     plus_bugs_err = mape(plus_bugs_predictions, modelin[!, eeg_feat])
     plus_bugs_r² = cor(plus_bugs_predictions, modelin[!, eeg_feat])^2
-
-    bugs_only_err = mape(bugs_only_predictions, modelin[!, eeg_feat])
-    bugs_only_r² = cor(bugs_only_predictions, modelin[!, eeg_feat])^2
-
-    append!(summary_stats, DataFrame(
-        feature = fill(eeg_feat, 3),
-        model = ["age_only", "bugs_only", "age_plus_bugs"],
-        r² = [age_only_r², bugs_only_r², plus_bugs_r²],
-        mape = [age_only_err, bugs_only_err, plus_bugs_err]
-    ))
 
     fig = Figure(; size=(400, 600))
     ax1 = Axis(fig[1,1];
