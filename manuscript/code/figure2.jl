8228c3c5140b3c0ae416738786ebe8e08e35f10d
Kevin Bonham, PhD <kbonham@wellesley.edu>
2024-01-18 21:12:10 -0500

    set up lms on complete cohorts

--- manuscript/code/figure2.jl
+++ manuscript/code/figure2.jl
@@ -16,22 +16,26 @@ future_3m12m = load_cohort("future_3m12m")
 future_6m12m = load_cohort("future_6m12m")
 
 concurrent_3m_func = DataFrame(get(concurrent_3m))
+load_functional_profiles!(concurrent_3m_func);
 
-load_taxonomic_profiles!(mbo)
-load_functional_profiles!(mbo)
-unique!(mbo, :seqprep)
-sort!(mbo, "age")
-subset!(mbo, "age"=> ByRow(!ismissing))
+concurrent_6m_func = DataFrame(get(concurrent_6m))
+load_functional_profiles!(concurrent_6m_func);
 
-tps = ("3m", "6m", "12m")
-mbotps = Tuple((select(
-            unique(
-                subset(mbo, "visit"=> ByRow(v-> !ismissing(v) && v == "$(tp)o"), "subject"=> ByRow(!ismissing)),
-                "subject"
-            ), 
-            "subject", "age"=>"stool_age", "seqprep"=>"sample", "age", Cols(:))
-        for tp in tps
-))
+concurrent_12m_func = DataFrame(get(concurrent_12m))
+load_functional_profiles!(concurrent_12m_func);
+
+future_3m6m_func = DataFrame(get(future_3m6m))
+transform!(future_3m6m_func, AsTable(["age", "eeg_age"])=> ByRow(nt-> nt.eeg_age - nt.age)=> "age_diff")
+load_functional_profiles!(future_3m6m_func);
+
+future_3m12m_func = DataFrame(get(future_3m12m))
+load_functional_profiles!(future_3m12m_func);
+
+future_6m12m_func = DataFrame(get(future_6m12m))
+load_functional_profiles!(future_6m12m_func);
+
+##
+
 
 eeg_features = "peak_latency_" .* ["N1", "P1_corrected", "N2_corrected"]
 eeg_features = [eeg_features; replace.(eeg_features, "latency"=>"amp")]
@@ -39,4 +43,22 @@ eeg_features = [eeg_features; replace.(eeg_features, "latency"=>"amp")]
 na_map = FeatureSetEnrichments.get_neuroactive_unirefs()
 na_map_full = FeatureSetEnrichments.get_neuroactive_unirefs(; consolidate=false)
 
+
+for feature in eeg_features
+    EEGMicrobiome.runlms(concurrent_3m_func, "./data/outputs/lms/$(feature)_3m_lms.csv",
+                         feature, names(concurrent_3m_func, r"^UniRef")
+    )
+end
+
+for feature in eeg_features
+    EEGMicrobiome.runlms(concurrent_6m_func, "./data/outputs/lms/$(feature)_6m_lms.csv",
+                         feature, names(concurrent_6m_func, r"^UniRef")
+    )
+end
+
+for feature in eeg_features
+    EEGMicrobiome.runlms(concurrent_12m_func, "./data/outputs/lms/$(feature)_12m_lms.csv",
+                         feature, names(concurrent_12m_func, r"^UniRef")
+    )
+end
 
