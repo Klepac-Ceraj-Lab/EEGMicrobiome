91117fbfcb9d9d8ff0c98bfdad287e081bc21839
Kevin Bonham, PhD <kbonham@wellesley.edu>
2025-04-28 17:12:48 -0400

    test linear model assumptions

--- manuscript/code/figure2_3.jl
+++ manuscript/code/figure2_3.jl
@@ -100,6 +100,16 @@ eeg_features = [eeg_features; replace.(eeg_features, "latency" => "amp")]
 na_map = FeatureSetEnrichments.get_neuroactive_unirefs()
 na_map_full = FeatureSetEnrichments.get_neuroactive_unirefs(; consolidate=false)
 
+# Response to mBio reviwer 1 - investigating whether there is equivalence between
+# eeg / mgx as dep/indep variable
+for feature in eeg_features[1:1]
+    @info feature
+    EEGMicrobiome.run_reverse_lms(v1_func, "./data/outputs/lms/$(feature)_reverse_v1_lms.csv",
+        feature, names(v1_func, r"^UniRef"); age_col="stool_age"
+    )
+end
+
+
 for feature in eeg_features
   @info feature
   EEGMicrobiome.runlms(v1_func, "./data/outputs/lms/$(feature)_nodiff_v1_lms.csv",
@@ -156,7 +166,7 @@ for feature in eeg_features
   )
 end
 
-for feature in eeg_features
+or feature in eeg_features
   @info feature
   EEGMicrobiome.runlms(v1v3_func, "./data/outputs/lms/$(feature)_v1v3_lms.csv", feature, names(v1v3_func, r"^UniRef");
     additional_cols=[:age_diff], age_col="stool_age",
